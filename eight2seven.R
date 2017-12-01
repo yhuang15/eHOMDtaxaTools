00a40ba422d7fa645a4026f72bd1c3b56e2bc543
Yanmei Huang <yhuang@forsyth.org>
2017-11-30 22:30:52 -0500

    modified:   eight2seven.R         Modified the eightTOseven() function to accommodate the situation         where "outputBootstraps = FALSE" was used in the assignTaxonomy()         step and the resulting taxa object contains only taxonomy and no         bootstrap values, which is actually the default setting of the         assignTaxonomy() function.

--- eight2seven.R
+++ eight2seven.R
@@ -1,28 +1,33 @@
 ######################################################################################
 ####
 #### Description: This R function is needed to format the output object of the DADA2 
-####              package's "assignTaxonomy" function when the eHOMD RDP classifier
+####              package's "assignTaxonomy()" function when the eHOMD RDP classifier
 ####              training dataset is used for assigning Taxonomy.  Without using this
-####              function, the object produced by "assignTaxonomy" using eHOMD
+####              function, the object produced by "assignTaxonomy()" using eHOMD
 ####              training dataset contains 8 levels of taxonomy, i.e. there is an 
 ####              additional "SuperSpecies" level above the "Species" level.  This
 ####              function merge "SuperSpecies" with "Species", replacing cases where
 ####              "NA"s are assigned in "Species" with the assignment and corresponding
 ####              bootstrap value at "SuperSpecies" level.
-####              
+####
 ####               
 #### Author:      Yanmei Huang
-#### Version:     1.0
-#### Date:        2017-11-29
+#### Version:     1.1
+#### Date:        2017-11-30
 ####
 ######################################################################################
 
 eightTOseven = function(taxa)
 {
-  taxa$boot[is.na(taxa$tax[, 'Species']), 'Species'] = taxa$boot[is.na(taxa$tax[, 'Species']), 'SuperSpecies']
-  taxa$tax[is.na(taxa$tax[, 'Species']), 'Species'] = taxa$tax[is.na(taxa$tax[, 'Species']), 'SuperSpecies']
-  taxa$tax = cbind(taxa$tax[, 1:6], taxa$tax[, 8])
-  taxa$boot = cbind(taxa$boot[, 1:6], taxa$boot[, 8])
+  if (length(names(classified)) > 0)
+  {
+    taxa$boot[is.na(taxa$tax[, 'Species']), 'Species'] = taxa$boot[is.na(taxa$tax[, 'Species']), 'SuperSpecies']
+    taxa$tax[is.na(taxa$tax[, 'Species']), 'Species'] = taxa$tax[is.na(taxa$tax[, 'Species']), 'SuperSpecies']
+    taxa$tax = cbind(taxa$tax[, 1:6], taxa$tax[, 8])
+    taxa$boot = cbind(taxa$boot[, 1:6], taxa$boot[, 8])
+  } else {
+    taxa[is.na(taxa[, 'Species']), 'Species'] = taxa[is.na(taxa[, 'Species']), 'SuperSpecies']
+    taxa = cbind(taxa[, 1:6], taxa[, 8])
+  }
   return (taxa)
 }
-
