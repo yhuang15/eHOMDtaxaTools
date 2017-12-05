3d563c81e00eb9f196e69c0cf8328d921fe1dddc
Yanmei Huang <yhuang@forsyth.org>
2017-12-05 08:15:58 -0500

    modified:   classifyMEDout.R 	The first version of classifyMEDout.R was modified from Isabel's 	Rmd file. I now rewrite to get rid of unnecessary R packages, 	deal with warnings, and make the valuable names more general.

--- classifyMEDout.R
+++ classifyMEDout.R
@@ -3,7 +3,7 @@
 ######################################################################################
 ####
 #### Description: This script runs RDP classifier on MED output fasta using a
-####              training data set
+####              eHOMD training data set
 ####
 #### Usage:       classifyMEDout.R MEDoutfile trainingset_file
 ####               
@@ -30,22 +30,17 @@ Tfile = as.character(commandArgs()[7 - offset])
 dis = gsub(".*Taxa_", "", Tfile)
 dis = gsub(".fa.gz", "", dis)
 
-
-
-####{r, warning=FALSE, message=FALSE, load required packages}
-library(tidyverse); packageVersion("tidyverse")
+####{r load required packages}
 library(Biostrings); packageVersion("Biostrings")
-library(readr); packageVersion("readr")
-library(stringr); packageVersion("stringr")
 library(dada2); packageVersion("dada2")
 
 ####{r read fasta file}
 
-nodes = readDNAStringSet(infile)
+MEDnodes = readDNAStringSet(infile)
 
 ####{r run RDP Classifier}
 
-SuperTaxa_8levels = assignTaxonomy(paste(nodes), 
+classified = assignTaxonomy(paste(MEDnodes), 
                                    Tfile, 
                                    outputBootstraps=TRUE, 
                                    taxLevels=c("Kingdom", "Phylum", 
@@ -54,25 +49,25 @@ SuperTaxa_8levels = assignTaxonomy(paste(nodes),
                                                "Species"))
 
 ####{r format output table}
-SuperTaxa_8levels = as.data.frame(cbind(select(as.data.frame(SuperTaxa_8levels$tax), 
-                                               Kingdom, Phylum, Class, Order, Family, 
-                                               Genus, SuperSpecies, Species), 
-                                        select(as.data.frame(SuperTaxa_8levels$boot), 
-                                               Kingdom, Phylum, Class, Order, Family, 
-                                               Genus, SuperSpecies, Species)))
-colnames(SuperTaxa_8levels) = c("Kingdom","Phylum","Class","Order","Family","Genus",
-                                "SuperSpecies","Species", "Kingdom.B","Phylum.B",
+classified = cbind(as.data.frame(classified$tax)[, c('Kingdom', 'Phylum', 'Class', 
+                                                     'Order', 'Family', 
+                                                     'Genus', 'Species')], 
+                  as.data.frame(classified$boot)[, c('Kingdom', 'Phylum', 'Class', 
+                                                     'Order', 'Family', 
+                                                     'Genus', 'Species')],
+                  row.names = names(MEDnodes))
+                                 
+colnames(classified) = c("Kingdom","Phylum","Class","Order","Family","Genus",
+                                "Species", "Kingdom.B","Phylum.B",
                                 "Class.B","Order.B","Family.B","Genus.B",
-                                "SuperSpecies.B","Species.B")
+                                "Species.B")
 
-rownames(SuperTaxa_8levels) = names(nodes)
-SuperTaxa_8levels$Reads = gsub(".*size:", "", names(nodes))
-SuperTaxa_8levels$Reads = as.integer(SuperTaxa_8levels$Reads)
+classified$Reads = gsub(".*size:", "", names(MEDnodes))
+classified$Reads = as.integer(classified$Reads)
 
 ####{r write output file}
 outfile = paste0(gsub(".fasta", "", infile), ".classified.", dis, ".csv")
-write.csv(SuperTaxa_8levels, file = outfile, quote = FALSE)
-
+write.csv(classified, file = outfile, quote = FALSE)
 
 message()
 message(paste0("Analysis finished at ", Sys.time()))
